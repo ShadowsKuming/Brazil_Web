8fb5e2f0c37df02b5f6d0ebe189a755c11d501f9
J.Cheng <shadowscheng@gmail.com>
2026-01-06 13:33:52 +0800

    renew title and web icon

--- src/router/index.js
+++ src/router/index.js
@@ -6,33 +6,49 @@ const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
 
   routes: [
-    { path: '/', name: 'home', component: HomeView },
+    { 
+      path: '/', 
+      name: 'home', 
+      component: HomeView,
+      meta: {title: 'Brazil Research | Home' }, 
+    },
     {
       path: '/prototypes',
       name: 'prototypes',
       component: () => import('../views/Prototypes.vue'),
+      meta: {title: 'Brazil Research | Prototypes' },
     },
     {
       path: '/people',
       name: 'people',
       component: () => import('../views/AboutView.vue'),
+      meta: {title: 'Brazil Research | People' },
     },
     {
       path: '/contact',
       name: 'contact',
       component: () => import('../views/ContactView.vue'),
+      meta: {title: 'Brazil Research | Contact' },
     },
     {
       path: '/news',
       name: 'news',
       component: () => import('../views/NewsEvents.vue'),
+      meta: {title: 'Brazil Research | News & Events' },
     },
     {
       path: '/projects',
       name: 'projects',
       component: () => import('@/views/ProjectsView.vue'),
+      meta: {title: 'Brazil Research | Projects' },
     },
   ],
 })
+
+router.afterEach((to) => {
+  const baseTitle = 'Brazil Research Lab'
+  document.title = to.meta.title || baseTitle
+})
+
 
 export default router
